b1827c19592e6c90493a0bd850d2b7669790fb92
Дмитрий Гридин <msdima6@gmail.com>
2020-10-18 23:06:32 +0300

    The option '--no-hidden' wad added

--- fcount/dircount.cpp
+++ fcount/dircount.cpp
@@ -3,6 +3,7 @@
 #include "HelpPrinter.h"
 #include <filesystem>
 #include <regex>
+#include <Windows.h> 
 
 using std::cout;
 using std::endl;
@@ -17,7 +18,10 @@ const string _EMPTY_STRING_ = "";
 const string _ERROR_PREFIX_ = "Error: ";
 
 template<typename _Iter>
-void directory_traversal(const string& _dir, size_t& _counter, bool _empty_too, int _dispay_count, string _mask);
+void directory_traversal(const string& _dir, size_t& _counter, bool _empty_too, bool _hidden_too, int _dispay_count, string _mask);
+
+bool is_hidden(const string& path);
+const wchar_t* get_wchar_t(const char* ch);
 
 int main(int argc, const char* argv[]) 
 {
@@ -29,6 +33,7 @@ int main(int argc, const char* argv[])
 		CommandLineOption("help", "h", "print this help message"),
 		CommandLineOption("show", "s", "display the first 'n' directories", false, "n", "10"),
 		CommandLineOption("no-empty", "", "empty directories are not counted"), 
+		CommandLineOption("no-hidden", "", "hidden directories are not counted"), 
 		CommandLineOption("pattern", "p", "the mask that the directory name must match", false, "mask"),
 		CommandLineOption("directory", "d", "the directory this program should work with", false, "path")
 	};
@@ -66,6 +71,8 @@ int main(int argc, const char* argv[])
 	try
 	{
 		bool empty = !parsed_options.has_option("no-empty");
+		bool hidden = !parsed_options.has_option("no-hidden");
+
 		size_t pattern_option_index = parsed_options.index_by("pattern");
 
 		string mask = (pattern_option_index != parsed_options.end()) ?
@@ -79,14 +86,14 @@ int main(int argc, const char* argv[])
 
 		if (parsed_options.has_option("recursive"))  
 		{
-			directory_traversal<rdir_it>(work_directory, directories_count, empty, display_count, mask);
+			directory_traversal<rdir_it>(work_directory, directories_count, empty, hidden, display_count, mask);
 		}
 		else 
 		{
-			directory_traversal<dir_it>(work_directory, directories_count, empty, display_count, mask);
+			directory_traversal<dir_it>(work_directory, directories_count, empty, hidden, display_count, mask);
 		}
 
-		cout << endl << "The directory " << absolute(work_directory) << " contains " << directories_count << " folders." << endl;
+		cout << endl << "The directory " << absolute(work_directory) << " contains " << directories_count << " folders." << endl; 
 	}
 	catch (const std::exception& e)
 	{
@@ -97,37 +104,56 @@ int main(int argc, const char* argv[])
 	if (parsed_options.has_option("help"))
 	{
 		cout << endl; 
-		help_printer.print();
+		help_printer.print(); 
 		cout << endl;
 	}
 }
 
 
 template<typename _Iter>
-void directory_traversal(const string & _dir, size_t & _counter, bool _empty_too, int _dispay_count, string _mask)
+void directory_traversal(const string & _dir, size_t & _counter, bool _empty_too, bool _hidden_too, int _dispay_count, string _mask)
 {
 	if (_dispay_count > 0)
 	{
 		cout << endl << "File names: " << endl << endl;
 	}
 
-	basic_regex regex(_mask);
+	basic_regex regex(_mask); 
 
-	for (auto file : _Iter(_dir))
+	for (auto file : _Iter(_dir)) 
 	{ 
 		bool is_directory = file.is_directory();
 		bool is_dir_empty = is_empty(file.path());
-		bool has_mask = (_mask != _EMPTY_STRING_) ? true : false;
+		bool has_mask = (_mask != _EMPTY_STRING_) ? true : false; 
 		bool matching = regex_match(file.path().filename().string(), regex);
 		 
-		if (is_directory && (_empty_too || (!_empty_too && !is_dir_empty))
-			&& (!has_mask || (has_mask && matching)))
+		if (is_directory && (_empty_too || !is_dir_empty)
+			&& (_hidden_too || !is_hidden(file.path().string()))
+			&& (!has_mask || matching))
 		{
 			++_counter;
 			if (_dispay_count-- > 0)
 			{ 
-				cout << _counter << ". " << file.path() << '\n';
+				cout << _counter << ". " << file.path() << '\n';   
 			}
 		}
 	}
+}
+
+bool is_hidden(const string& path)
+{
+	LPCWSTR wch_path = get_wchar_t(path.c_str());
+	DWORD attr = GetFileAttributes(wch_path);
+	return ((attr & FILE_ATTRIBUTE_HIDDEN) != 0);
+}
+
+const wchar_t* get_wchar_t(const char* ch)
+{
+	const size_t size = strlen(ch) + 1;
+	wchar_t* wch = new wchar_t[size];
+
+	size_t out_size;
+	mbstowcs_s(&out_size, wch, size, ch, size - 1);
+
+	return wch;
 }
